7d0e2abe1a3c65319243d098c7c0765697f1be88
mdebanshu <iamdebanshumukhopadhay@gmail.com>
2024-01-15 13:21:30 +0530

    test

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import os
+import openpyxl
+import sqlite3
+
+
+window = tk.Tk()
+
+window.geometry('500x500')
+
+window.title('Login CRED')
+
+frame = tk.Frame(window)
+
+frame.pack()
+window.mainloop()
+
